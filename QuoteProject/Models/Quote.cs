eceadf9f13f15b4b935c7233d85e33f5800e69fa
Miroslav Maděra <u17.415@pslib.cloud>
2020-10-01 13:23:06 +0200

    first

---
+++ QuoteProject/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteProject.Models
+{
+    public class Quote
+    {
+        public string Text { get; set; }
+        public DateTime DateofPost { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
