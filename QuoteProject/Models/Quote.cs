66d85325d6c923c425bd3108475796a332fa5663
Miroslav Maděra <u17.415@pslib.cloud>
2020-10-01 13:34:49 +0200

    second

--- QuoteProject/Models/Quote.cs
+++ QuoteProject/Models/Quote.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace QuoteProject.Models
 {
     public class Quote
     {
+        [Key]
+        public int ID { get; set; }
         public string Text { get; set; }
         public DateTime DateofPost { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+        [JsonIgnore]
+        public ICollection<QuoteTag> QuoteTags { get; set; }
     }
 }
--- QuoteProject/Models/Tag.cs
+++ QuoteProject/Models/Tag.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace QuoteProject.Models
 {
     public class Tag
     {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public ICollection<QuoteTag> QuoteTags { get; set; }
+    }
+    public enum Category
+    {
+        prvni,
+        druhy,
+        treti,
+        ctvrty
     }
 }
